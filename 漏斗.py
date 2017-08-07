902b493043c968e356c94211b732e1cf9a012469
sandy-pan <30787419+Sandy-Pan@users.noreply.github.com>
2017-08-07 17:04:34 +0800

    Add files via upload

---
+++ 漏斗.py
@@ -0,0 +1,36 @@
+n = 5
+
+for i in range(n):
+  for j in range(i):
+    print(" ",end="")
+  for a in range(2*(n-i)-1):
+    print("*",end="")
+
+  print("")
+
+
+
+
+
+
+
+
+for i in range(n):
+  # print(i, 4-i, 1+2*i)
+  for j in range(4-i):
+    print(" ", end="")
+  for j in range(1+2*i):
+    print("*", end="")
+  print("")
+
+
+
+
+
+
+
+
+    
+
+
+ 
